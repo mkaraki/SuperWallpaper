38a47051ba4cf710c1c05cb108c329928fcd49a1
mkaraki <mkaraki_apps_publisher@outlook.jp>
2018-11-13 23:00:57 +0900

    Add Readme and License Information

--- SuperWallpaper/Program.cs
+++ SuperWallpaper/Program.cs
@@ -8,7 +8,9 @@ namespace SuperWallpaper
 {
     public static class Program
     {
-        public static string conf_file = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),"SpecialWallpaper.conf");
+        public static string conf_file = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),"SuperWallpaper.txt");
+        public static string app_dir = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+        public static string readme_url = "file:///" + System.IO.Path.GetFullPath(System.IO.Path.Combine(app_dir, "FactoryHTML", "readme.html")).Replace('\\','/');
 
         /// <summary>
         /// アプリケーションのメイン エントリ ポイントです。
@@ -17,7 +19,7 @@ namespace SuperWallpaper
         static void Main()
         {
             if (!System.IO.File.Exists(conf_file))
-                System.IO.File.WriteAllText(conf_file,"https://www.google.com",System.Text.Encoding.UTF8);
+                System.IO.File.WriteAllText(conf_file, readme_url, System.Text.Encoding.UTF8);
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
